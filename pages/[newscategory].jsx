cdb8d9d07e32413816b3cc5bc73eeaa01a0ef4f4
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-17 16:07:41 +0500

    dynamic things are done

---
+++ pages/[newscategory].jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function NewsCatergory({ articles, newscategory }) {
+  return (
+    <div>
+      <h1>News about {newscategory}</h1>
+      {/* {console.log(articles)} */}
+
+      {articles.map((article) => (
+        <div key={article.id}>
+          <>{article.title}</>
+          <>{article.content}</>
+        </div>
+      ))}
+      <hr />
+    </div>
+  );
+}
+
+export default NewsCatergory;
+
+export async function getServerSideProps(context) {
+  //   console.log("context", context);
+  const { params } = context;
+  const { newscategory } = params;
+  //   console.log("params", params);
+  const response = await fetch(
+    `http://localhost:4000/news/?category=${params.newscategory}`
+  );
+  const data = await response.json();
+  //   console.log(data);
+  return {
+    props: {
+      articles: data,
+      newscategory,
+    },
+  };
+}
