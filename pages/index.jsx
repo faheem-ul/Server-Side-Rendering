15b30232f570017f3c27a155750052aa65253d37
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-17 15:43:18 +0500

    SSR is happening

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
-function Home() {
+function Home({ news }) {
   return (
     <div>
       <h1> Server Side Rendering</h1>
+      {news.map((item, index) => {
+        return (
+          <div key={index}>
+            <h2>{item.category}</h2>
+            <p>
+              {item.title} | {item.content}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 export default Home;
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:4000/news");
+  const data = await response.json();
+  return {
+    props: {
+      news: data,
+    },
+  };
+}
